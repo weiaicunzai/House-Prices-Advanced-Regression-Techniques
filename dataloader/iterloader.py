6baa5026793d98e23c6efbea7bdff5bb2fe7bfa1
weiaicunzai <372995411@qq.com>
2022-11-26 20:12:17 +0800

    feat: add IterLoader class

---
+++ dataloader/iterloader.py
@@ -0,0 +1,22 @@
+# import time
+
+class IterLoader:
+
+    def __init__(self, dataloader):
+        self._dataloader = dataloader
+        self.iter_loader = iter(self._dataloader)
+
+    def __next__(self):
+        try:
+            data = next(self.iter_loader)
+        except StopIteration:
+            self.iter_loader = iter(self._dataloader)
+            data = next(self.iter_loader)
+
+        return data
+
+    def __len__(self):
+        return len(self._dataloader)
+
+    def __iter__(self):
+        return self
