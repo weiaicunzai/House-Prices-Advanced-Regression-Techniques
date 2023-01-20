f1bbccbce00833c6083aa51117cded5d31d19f41
weiaicunzai <372995411@qq.com>
2023-01-21 03:16:00 +0800

    feat: add LossVariance class

--- loss.py
+++ loss.py
@@ -273,6 +273,30 @@ def test_mask(path):
             #img = check_mask(gt, pred)
             #cv2.imwrite('result/{}.png'.format(idx), img)
 
+class LossVariance(nn.Module):
+    """ The instances in target should be labeled
+    """
+    def __init__(self):
+        super(LossVariance, self).__init__()
+
+    def forward(self, input, target):
+        B = input.size(0)
+
+        loss = 0
+        for k in range(B):
+            unique_vals = target[k].unique()
+            unique_vals = unique_vals[unique_vals != 0]
+
+            sum_var = 0
+            for val in unique_vals:
+                instance = input[k][:, target[k] == val]
+                if instance.size(1) > 1:
+                    sum_var += instance.var(dim=1).sum()
+
+            loss += sum_var / (len(unique_vals) + 1e-8)
+        loss /= B
+        return loss
+
 
 
 #test_mask('save')
