2a8fa8e213889f7b03a94e2549f0a81734942c26
weiaicunzai <372995411@qq.com>
2023-03-23 22:22:39 +0800

    feat: add histology datasets

---
+++ dataset/crag.py
@@ -0,0 +1,158 @@
+import os
+import zipfile
+import shutil
+import glob
+import re
+
+import cv2
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torchvision.datasets.utils import download_url
+import numpy as np
+
+class CRAG(Dataset):
+    def __init__(self, path, image_set, transforms=None):
+        # url = 'https://warwick.ac.uk/fac/sci/dcs/research/tia/glascontest/download/warwick_qu_dataset_released_2016_07_08.zip'
+        # file_name = 'warwick_qu_dataset_released_2016_07_08.zip'
+        # md5 = '495b2a9f3d694545fbec06673fb3f40f'
+        #self.weight_map_path = '/data/hdd1/by/FullNet-varCE/data/GlaS/weight_maps/train'
+        #self.weight_map_path_val = '/data/hdd1/by/FullNet-varCE/data/GlaS/weight_maps/val'
+        #self.weight_map_path = os.path.join(path, 'weight_maps', 'train')
+        self.weightmap_path = '/data/smb/syh/gland_segmentation/CRAGV2/weight_maps/'
+        #self.weight_map_path_val = os.path.join(path, 'weight_maps', 'val')
+
+        #if download:
+            #download_url(url, path, file_name, md5=md5)
+
+        self.class_names = ['background', 'gland']
+        #self.class_names = ['background', 'gland', 'cnt']
+        # self.ignore_index = -100
+        self.ignore_index = 255
+        self.class_num = len(self.class_names)
+
+        # data_folder = os.path.join(path, 'Warwick QU Dataset (Released 2016_07_08)')
+        # if not os.path.exists(data_folder):
+        #     if not os.path.exists(os.path.join(path, file_name)):
+        #         raise RuntimeError('Dataset not found or corrupted.' +
+        #                            ' You can use download=True to download it')
+
+        #     with zipfile.ZipFile(os.path.join(path, file_name), "r") as f:
+        #         f.extractall(path=path)
+
+        self.image_set = image_set
+        self.images = []
+        self.labels = []
+        self.weight_maps = []
+        if image_set == 'train':
+            search_path = os.path.join(path, image_set, '**', '*.png')
+        else:
+            search_path = os.path.join(path, 'valid', '**', '*.png')
+        # if image_set not in ['train', 'testA', 'testB', 'val']:
+            # raise ValueError('wrong image_set argument')
+        # label_re = image_set + '_[0-9]+_' + 'anno' + '\.bmp'
+        #label_re =  'anno' + '\.bmp'
+        # if image_set == 'val':
+            # label_re = 'test[A|B]' +  '_[0-9]+_' + 'anno' + '\.bmp'
+
+        self.image_names = []
+        for img_filename in glob.iglob(search_path, recursive=True):
+            if 'Images' not in img_filename:
+                continue
+
+            self.images.append(cv2.imread(img_filename, -1))
+            #if re.search(label_re, bmp):
+                #print(bmp)
+            segmap_filename = img_filename.replace('Images', 'Annotation')
+            self.labels.append(
+                    self.construct_contour(cv2.imread(segmap_filename, -1)))
+            #elif re.search(image_re, bmp):
+            basename = os.path.basename(img_filename)
+            self.image_names.append(basename)
+            weightmap_filename = os.path.join(self.weightmap_path, basename.replace('.png', '_weight.png'))
+            if image_set == 'train':
+                self.weight_maps.append(
+                    cv2.imread(weightmap_filename, -1))
+
+                #if self.image_set == 'train':
+                #    bs_name = os.path.basename(bmp)
+                #    bs_name = bs_name.replace('.bmp', '_anno_weight.png')
+                #    #if 'train' in bs_name:
+                #    weight_map_filename = os.path.join(
+                #            self.weight_map_path, bs_name)
+                #    #else:
+                #    #    weight_map_filename = os.path.join(
+                #    #        self.weight_map_path_val, bs_name)
+
+                #    #print(bmp, weight_map_filename)
+                #    self.weight_maps.append(
+                #        cv2.imread(weight_map_filename, -1)
+                #    )
+
+        assert len(self.images) == len(self.labels)
+        self.transforms = transforms
+        self.mean = (0.8572969 , 0.72236917, 0.83065554),
+        self.std = (0.09965163, 0.17253199, 0.1335271)
+        self.times = 30000
+
+    def construct_contour(self, label):
+        if self.image_set == 'train' and self.class_num == 3:
+            kernel = np.ones((3, 3), np.uint8)
+            label[label > 0] = 1
+            label_tmp = label.copy()
+            label = cv2.erode(label, kernel, iterations=4)
+            label_tmp[label_tmp > 0] = 1
+            cnt = label_tmp - label
+            label[cnt == 1] = 2
+
+            return label
+        elif self.image_set == 'train' and self.class_num == 2:
+            kernel = np.ones((3, 3), np.uint8)
+            label[label > 0] = 1
+            # prevent glands clustered together when resize the img to a smaller size
+            label = cv2.erode(label, kernel, iterations=1)
+
+            return label
+
+        else:
+            label[label > 0] = 1
+            return label
+
+    def __len__(self):
+        if self.image_set == 'train':
+            return len(self.images) * self.times
+        else:
+            return len(self.images)
+
+    def __getitem__(self, index):
+
+        if self.image_set == 'train':
+            index = index % len(self.images)
+
+        image = self.images[index]
+        label = self.labels[index]
+
+
+        if self.image_set != 'train':
+            img_meta = self.transforms(image, label)
+            #print(index, self.image_set, len(self), len(self.images))
+            img_meta['img_name'] = self.image_names[index]
+            return img_meta
+
+        else:
+            if self.transforms is not None:
+                weight_map = self.weight_maps[index]
+                #print(self.transforms)
+                image, label, weight_map = self.transforms(image, label, weight_map)
+                #h, w = label.shape
+                #weight_map = cv2.resize()
+                #weight_map = torch.from_numpy(weight_map)
+            return image, label, weight_map
+            #return image, label
+
+
+# import sys
+# sys.path.append(os.getcwd())
+# import utils
+# dataset = CRAG('/data/smb/syh/gland_segmentation/CRAGV2/CRAG/', image_set='train')
+# print(len(dataset))
---
+++ dataset/pre_training_dataset.py
@@ -0,0 +1,548 @@
+import os
+import glob
+import csv
+
+import numpy as np
+import cv2
+from torch.utils.data import Dataset
+
+import sys
+sys.path.append(os.getcwd())
+
+import scipy.io as sio
+
+datasets = {
+    'CoCaHis' : '/data/smb/syh/colon_dataset/CoCaHis/cocahis',
+    'GlasTrain' : '/data/smb/syh/gland_segmentation/Glas/Warwick QU Dataset (Released 2016_07_08)',
+    'GlasTest' : '/data/smb/syh/gland_segmentation/Glas/Warwick QU Dataset (Released 2016_07_08)',
+    'CRAGTrain' : '/data/smb/syh/gland_segmentation/CRAGV2/CRAG/',
+    'CRAGTest' : '/data/smb/syh/gland_segmentation/CRAGV2/CRAG/',
+    'EBHI' : '/data/smb/syh/gland_segmentation/EBHI-SEG/',
+    'ProstateSin' : '/data/smb/syh/gland_segmentation/Singapore/gland_seg/gland_segmentation_dataset/cropped_patches__complete_and_partial_glands_50_50_512_tars/imgs',
+    'RINGS' : '/data/smb/syh/gland_segmentation/RINGS',
+}
+
+class BaseDataset(Dataset):
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        self.path = path
+        self.repeat_factor = repeat_factor
+        self.return_label = return_label
+        self.binary = binary
+
+        img_infos = self.read_img_infos()
+        self.img_infos = self.expand_img_infos(img_infos)
+
+    def expand_img_infos(self, img_infos):
+
+        for _ in range(self.repeat_factor - 1):
+            img_infos.extend(img_infos)
+
+        return img_infos
+
+    def read_img_infos(self, path):
+        """ return two lists: img_names,  seg_maps """
+        raise NotImplementedError
+
+    def read_img(self, img_filename):
+        raise NotImplementedError
+
+    def read_segmap(self, segmap_filename):
+        raise NotImplementedError
+
+    def read_label(self, file_idx):
+        raise NotImplementedError
+
+    def __len__(self):
+        return len(self.img_infos)
+
+    def __getitem__(self, idx):
+        img_info = self.img_infos[idx]
+        #img_filename = self.img_filenames[idx]
+        #segmap_filename = self.segmap_filenames[idx]
+        img = self.read_img(img_info['img_filename'])
+
+        if self.return_label is None:
+            return img
+
+        elif self.return_label == 'pixel':
+            segmap = self.read_segmap(img_info['label'])
+            if segmap is None:
+                print(img_info['label'])
+
+            if self.binary:
+                segmap[segmap > 0] = 1
+
+            return img, segmap
+
+        elif self.return_label == 'img':
+            cls_idx = self.read_label(idx)
+            if not self.binary:
+                #raise ValueError('{} does not support img level non-binary labels'.format(self))
+                return img, cls_idx
+            else:
+                if idx > 0:
+                    return img, 1 # cancer
+                else:
+                    return img, 0
+
+        else:
+            raise ValueError('wrong value error')
+
+
+
+############
+# segmentation datasets
+class CoCaHis(BaseDataset):
+
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        super().__init__(path, repeat_factor=repeat_factor, return_label=return_label, binary=binary)
+
+    def read_img_infos(self):
+         """ return two lists: img_names,  seg_maps """
+         img_infos = []
+
+
+         for img in glob.iglob(os.path.join(self.path, '**', '*.jpg'), recursive=True):
+            img_info = {}
+            assert os.path.isfile(img)
+            segmap = img.replace('images', 'labels').replace('.jpg', '.png')
+            assert os.path.isfile(segmap)
+
+            img_info['img_filename'] = img
+            img_info['label'] = segmap
+            img_infos.append(img_info)
+
+         return img_infos
+
+    def read_label(self, idx):
+        # normal vs cancer
+        return 1
+
+    def read_img(self, img_filename):
+        img = cv2.imread(img_filename)
+        return img
+
+    def read_segmap(self, segmap_filename):
+        segmap = cv2.imread(segmap_filename, -1)
+        return segmap
+
+
+class GlasTrain(BaseDataset):
+
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        super().__init__(path, repeat_factor=repeat_factor, return_label=return_label, binary=binary)
+
+    def read_img_infos(self):
+         """ return two lists: img_names,  seg_maps """
+         img_infos = []
+
+
+         for img in glob.iglob(os.path.join(self.path, '**', '*.bmp'), recursive=True):
+            basename = os.path.basename(img)
+            if 'train' not in basename:
+                continue
+
+            if '_anno' in img:
+                continue
+
+            img_info = {}
+            assert os.path.isfile(img)
+            #segmap = img.replace('images', 'labels').replace('.jpg', '.png')
+            segmap = img.replace('.bmp', '_anno.bmp')
+            assert os.path.isfile(segmap)
+
+            img_info['img_filename'] = img
+            img_info['label'] = segmap
+            img_infos.append(img_info)
+
+         return img_infos
+
+    def read_label(self, idx):
+        if not hasattr(self, 'cls_labels'):
+            self.cls_labels = {}
+            with open(os.path.join(self.path, 'Grade.csv')) as csv_file:
+                reader = csv.DictReader(csv_file)
+                for row in reader:
+                    #name,
+                    self.cls_labels[row['name'] + '.bmp'] = row[' grade (GlaS)']
+                    #print(row['name'], row[' grade (GlaS)'])
+                    #print(row)
+                    #print(row.split(','))
+
+        img_info = self.img_infos[idx]
+        img_filename = img_info.get('img_filename')
+        #print(img_filename)
+        basename = os.path.basename(img_filename)
+        cls_name = self.cls_labels[basename]
+        if cls_name.strip() == 'benign':
+            return 0
+        else:
+            return 1
+
+    def read_img(self, img_filename):
+        img = cv2.imread(img_filename)
+        return img
+
+    def read_segmap(self, segmap_filename):
+        segmap = cv2.imread(segmap_filename, -1)
+        return segmap
+
+
+class GlasTest(BaseDataset):
+
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        super().__init__(path, repeat_factor=repeat_factor, return_label=return_label, binary=binary)
+
+    def read_img_infos(self):
+         """ return two lists: img_names,  seg_maps """
+         img_infos = []
+
+
+         for img in glob.iglob(os.path.join(self.path, '**', '*.bmp'), recursive=True):
+            basename = os.path.basename(img)
+            if 'test' not in basename:
+                continue
+
+            if '_anno' in img:
+                continue
+
+            img_info = {}
+            assert os.path.isfile(img)
+            #segmap = img.replace('images', 'labels').replace('.jpg', '.png')
+            segmap = img.replace('.bmp', '_anno.bmp')
+            assert os.path.isfile(segmap)
+
+            img_info['img_filename'] = img
+            img_info['label'] = segmap
+            img_infos.append(img_info)
+
+         return img_infos
+
+    def read_label(self, idx):
+        if not hasattr(self, 'cls_labels'):
+            self.cls_labels = {}
+            with open(os.path.join(self.path, 'Grade.csv')) as csv_file:
+                reader = csv.DictReader(csv_file)
+                for row in reader:
+                    #name,
+                    self.cls_labels[row['name'] + '.bmp'] = row[' grade (GlaS)']
+                    #print(row['name'], row[' grade (GlaS)'])
+                    #print(row)
+                    #print(row.split(','))
+
+        img_info = self.img_infos[idx]
+        img_filename = img_info.get('img_filename')
+        #print(img_filename)
+        basename = os.path.basename(img_filename)
+        cls_name = self.cls_labels[basename]
+        if cls_name.strip() == 'benign':
+            return 0
+        else:
+            return 1
+
+    def read_img(self, img_filename):
+        img = cv2.imread(img_filename)
+        return img
+
+    def read_segmap(self, segmap_filename):
+        segmap = cv2.imread(segmap_filename, -1)
+        return segmap
+
+
+class CRAGTrain(BaseDataset):
+
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        super().__init__(path, repeat_factor=repeat_factor, return_label=return_label, binary=binary)
+
+    def read_img_infos(self):
+         """ return two lists: img_names,  seg_maps """
+         img_infos = []
+
+
+         for img in glob.iglob(os.path.join(self.path, '**', '*.png'), recursive=True):
+
+            basename = os.path.basename(img)
+            if 'train' not in basename:
+                continue
+
+            if 'Annotation' in img:
+                continue
+
+            if 'Overlay' in img:
+                continue
+
+            img_info = {}
+            assert os.path.isfile(img)
+            segmap = img.replace('Images', 'Annotation')
+            assert os.path.isfile(segmap)
+
+            img_info['img_filename'] = img
+            img_info['label'] = segmap
+            img_infos.append(img_info)
+
+         return img_infos
+
+    def read_label(self, idx):
+        raise ValueError('no img level label available')
+
+
+    def read_img(self, img_filename):
+        img = cv2.imread(img_filename)
+        return img
+
+    def read_segmap(self, segmap_filename):
+        segmap = cv2.imread(segmap_filename, -1)
+        return segmap
+
+class CRAGTest(BaseDataset):
+
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        super().__init__(path, repeat_factor=repeat_factor, return_label=return_label, binary=binary)
+
+    def read_img_infos(self):
+         """ return two lists: img_names,  seg_maps """
+         img_infos = []
+
+
+         for img in glob.iglob(os.path.join(self.path, '**', '*.png'), recursive=True):
+
+            basename = os.path.basename(img)
+            if 'test' not in basename:
+                continue
+
+            if 'Annotation' in img:
+                continue
+
+            if 'Overlay' in img:
+                continue
+
+            img_info = {}
+            assert os.path.isfile(img)
+            segmap = img.replace('Images', 'Annotation')
+            assert os.path.isfile(segmap)
+
+            img_info['img_filename'] = img
+            img_info['label'] = segmap
+            img_infos.append(img_info)
+
+         return img_infos
+
+    def read_label(self, idx):
+        raise ValueError('no img level label available')
+
+
+    def read_img(self, img_filename):
+        img = cv2.imread(img_filename)
+        return img
+
+    def read_segmap(self, segmap_filename):
+        segmap = cv2.imread(segmap_filename, -1)
+        return segmap
+
+class EBHI(BaseDataset):
+
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        super().__init__(path, repeat_factor=repeat_factor, return_label=return_label, binary=binary)
+
+    def read_img_infos(self):
+         """ return two lists: img_names,  seg_maps """
+         img_infos = []
+
+
+         for img in glob.iglob(os.path.join(self.path, '**', '*.png'), recursive=True):
+
+            #basename = os.path.basename(img)
+            dir_name = os.path.basename(os.path.dirname(img))
+            if dir_name != 'image':
+                continue
+
+            # lack of label
+            if 'GTGT2012149-2-400-001.png' in img:
+                continue
+
+            if 'GT2012149-2-400-001.png'  in img:
+                continue
+
+
+            img_info = {}
+            assert os.path.isfile(img)
+            segmap = img.replace('image', 'label')
+            assert os.path.isfile(segmap)
+
+            img_info['img_filename'] = img
+            img_info['label'] = segmap
+            img_infos.append(img_info)
+
+         return img_infos
+
+    def read_label(self, idx):
+        raise ValueError('no img level label available')
+
+
+    def read_img(self, img_filename):
+        img = cv2.imread(img_filename)
+        return img
+
+    def read_segmap(self, segmap_filename):
+        segmap = cv2.imread(segmap_filename, -1)
+        return segmap
+
+
+
+class ProstateSin(BaseDataset):
+
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        super().__init__(path, repeat_factor=repeat_factor, return_label=return_label, binary=binary)
+
+    def read_img_infos(self):
+        """ return two lists: img_names,  seg_maps """
+        img_infos = []
+
+        for img in glob.iglob(os.path.join(self.path, '**', 'img', '*.png'), recursive=True):
+            img_info = {}
+
+            assert os.path.isfile(img)
+
+            segmap = img.replace('.png', '_mask.png')
+            #img =
+            #print(img)
+            segmap_dir = os.path.dirname(segmap)
+            segmap_dir = os.path.dirname(segmap_dir)
+            segmap_dir = os.path.join(segmap_dir, 'mask')
+            segmap = os.path.join(segmap_dir, os.path.basename(segmap))
+            #print(img)
+            #print(seg_map)
+            # imgs.append(img)
+            # seg_maps.append(segmap)
+            assert os.path.isfile(segmap)
+
+
+            img_info['img_filename'] = img
+            img_info['label'] = segmap
+            img_infos.append(img_info)
+
+        return img_infos
+
+    def read_label(self, idx):
+        raise ValueError('no img level label available')
+
+
+    def read_img(self, img_filename):
+        img = cv2.imread(img_filename)
+        return img
+
+    def read_segmap(self, segmap_filename):
+        segmap = cv2.imread(segmap_filename, -1)
+        return segmap
+
+
+class RINGS(BaseDataset):
+
+    def __init__(self, path, repeat_factor=1, return_label=None, binary=True):
+        super().__init__(path, repeat_factor=repeat_factor, return_label=return_label, binary=binary)
+
+    def read_img_infos(self):
+        """ return two lists: img_names,  seg_maps """
+        img_infos = []
+
+        #path = '/data/smb/syh/gland_segmentation/RINGS'
+        #imgs = []
+        #seg_maps = []
+        for img in glob.iglob(os.path.join(self.path, '**', 'IMAGES', '*.png'), recursive=True):
+            img_info = {}
+            # imgs.append(img)
+            segmap = img.replace('IMAGES', 'MANUAL GLANDS')
+            # seg_maps.append(seg_map)
+            assert os.path.isfile(segmap)
+            img_info['img_filename'] = img
+            img_info['label'] = segmap
+            img_infos.append(img_info)
+            assert os.path.isfile(segmap)
+
+        #path = '/data/smb/syh/gland_segmentation/Singapore/gland_seg/gland_segmentation_dataset/cropped_patches__complete_and_partial_glands_50_50_512_tars/imgs'
+        #path = '/data/smb/syh/gland_segmentation/RINGS'
+        #for img in glob.iglob(os.path.join(path, '**', 'img', '*.png'), recursive=True):
+        #    img_info = {}
+
+        #    assert os.path.isfile(img)
+
+        #    segmap = img.replace('.png', '_mask.png')
+        #    #img =
+        #    #print(img)
+        #    segmap_dir = os.path.dirname(segmap)
+        #    segmap_dir = os.path.dirname(segmap_dir)
+        #    segmap_dir = os.path.join(segmap_dir, 'mask')
+        #    segmap = os.path.join(segmap_dir, os.path.basename(segmap))
+        #    #print(img)
+        #    #print(seg_map)
+        #    # imgs.append(img)
+        #    # seg_maps.append(segmap)
+        #    assert os.path.isfile(segmap)
+
+
+        #    img_info['img_filename'] = img
+        #    img_info['label'] = segmap
+        #    img_infos.append(img_info)
+
+        return img_infos
+
+    def read_label(self, idx):
+        raise ValueError('no img level label available')
+
+
+    def read_img(self, img_filename):
+        img = cv2.imread(img_filename)
+        return img
+
+    def read_segmap(self, segmap_filename):
+        segmap = cv2.imread(segmap_filename, -1)
+        return segmap
+
+
+    def get_prostate_rings(self):
+        path = '/data/smb/syh/gland_segmentation/RINGS'
+        imgs = []
+        seg_maps = []
+        for img in glob.iglob(os.path.join(path, '**', 'IMAGES', '*.png'), recursive=True):
+            imgs.append(img)
+            seg_map = img.replace('IMAGES', 'MANUAL GLANDS')
+            seg_maps.append(seg_map)
+
+        return imgs, seg_maps
+
+
+
+
+
+
+
+
+
+
+
+# #cocahis = ProstateSin(datasets.get('ProstateSin'), repeat_factor=1, return_label='pixel', binary=True)
+# cocahis = RINGS(datasets.get('RINGS'), repeat_factor=1, return_label='pixel', binary=True)
+# #cocahis = CRAGTrain(datasets.get('CRAGTrain'), repeat_factor=1, return_label='pixel', binary=True)
+cocahis = EBHI(datasets.get('EBHI'), repeat_factor=1, return_label='pixel', binary=True)
+
+for i in cocahis:
+    pass
+# #
+# #
+# #
+# ## test code
+# #
+# import random
+# img, segmap = random.choice(cocahis)
+# print(len(cocahis), np.unique(segmap))
+# #
+# segmap[segmap > 0] = 255
+# #h, w = img.shape[:2]
+# #h = int(h / 4)
+# #w = int(w / 4)
+
+# img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+# segmap = cv2.resize(segmap, (0, 0), fx=0.25, fy=0.25)
+# #
+# cv2.imwrite('test.jpg', img)
+# cv2.imwrite('test.png', segmap)
