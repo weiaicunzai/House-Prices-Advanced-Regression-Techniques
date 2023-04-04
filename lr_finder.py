b020ea0416eb84fc06f6a4fc98a31825a683a603
weiaicunzai <372995411@qq.com>
2023-04-04 20:19:03 +0800

    feat: add lr_finder.py to find optimal lr

--- lr_finder.py
+++ lr_finder.py
@@ -1,176 +1,182 @@
 import argparse
+import os
+import time
+import re
+import sys
+sys.path.append(os.getcwd())
+import skimage.morphology as morph
 
-import matplotlib.pyplot as plt
-import numpy as np
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+from torch.cuda.amp import GradScaler
+from torch.cuda.amp import autocast
+# from torch.optim.lr_scheduler import PolynomialLR
+import cv2
 
 import transforms
-from conf import settings
-from dataset.camvid import CamVid
-from lr_scheduler import ExponentialLR
 import utils
+from conf import settings
+# from dataset.voc2012 import VOC2012Aug
+from dataloader import IterLoader
+import test_aug
+from losses import DiceLoss, WeightedLossWarpper, GlandContrastLoss
+import sampler as _sampler
 
 
-def lr_finder(train_loader: DataLoader,
-              net: nn.Module,
-              start_lr: float = 1e-7,
-              end_lr: float = 10,
-              num_it: int = 100,
-              stop_div: bool = True,
-              smooth_f: float = 0.05,
-              weight_decay: float = 0
-              ):
-    """Performs the learning rate range test.
-    Arguments:
-        train_loader (torch.utils.data.DataLoader): the training set data laoder.
-        start_lr: the minimum learning rate to start: Default: 1e-7
-        end_lr (float, optional): the maximum learning rate to test. Default: 10.
-        num_iter (int, optional): the number of iterations over which the test
-            occurs. Default: 100.
-        stop_div (bool, optional): the test is stopped when the loss diverges.
-        smooth_f (float, optional): the loss smoothing factor within interval
-            [0, 1]. Disabled if set to 0, otherwise the loss is smoothed using
-            exponential smoothing. Details: 0.05.
-        weight_decay: (float, optional): weight_decay factor
 
-    Returns:
-        loss (numpy.array): loss for each iteration
-        lr (numpy.array): learning rate for each iteration
-    """
-
-    optimizer = optim.AdamW(net.parameters(), lr=start_lr,
-                            weight_decay=weight_decay)
-    exponetial_scheduler = ExponentialLR(optimizer, args.end_lr, args.num_it)
-    loss_fn = nn.CrossEntropyLoss(ignore_index=train_dataset.ignore_index)
-
-    losses = []
-    lrs = []
-    stop = False
-    count = 0
-
-    for _ in range(1, args.num_it):
-
-        if stop:
-            break
-
-        net.train()
-        for images, masks in train_loader:
-
-            count += 1
-            if count > args.num_it:
-                stop = True
-                break
-
-            optimizer.zero_grad()
-
-            if args.gpu:
-                images = images.cuda()
-                masks = masks.cuda()
-            preds = net(images)
-
-            loss = loss_fn(preds, masks)
-            loss.backward()
-
-            if torch.isnan(loss):
-                stop = True
-                break
-
-            optimizer.step()
-            exponetial_scheduler.step()
-
-            print('iteration: {}, lr: {:08f}, loss: {:04f}'.format(
-                count, optimizer.param_groups[0]['lr'], loss))
-
-            if count != 1:
-                loss = smooth_f * loss + (1 - smooth_f) * losses[-1]
-
-            losses.append(loss)
-            lrs.append(optimizer.param_groups[0]['lr'])
-
-    # plot the result
-    loss = np.array(losses[args.skip_start: -args.skip_end])
-    lr = np.array(lrs[args.skip_start: -args.skip_end])
-
-    return loss, lr
+from ignite.contrib.handlers import ProgressBar
+from ignite.engine.engine import Engine
+from ignite.handlers import FastaiLRFinder
 
 
-def plot(loss, lr, skip_start=10, skip_end=5, image_name='lr_finder.jpg'):
-    """Draw learning range test result
 
-    Args:
-        loss (numpy.array): loss data for each iteration
-        lr (numpy.array): learning rate data for each iteration
-        skip_start: number of iterations to trim from start
-        skip_end: number of iterations to trim from end
-        image_name: image path
-    """
 
-    plt.plot(lr, loss)
-    plt.xscale("log")
-    plt.xlabel("Learning rate")
-    plt.ylabel("Loss")
-    plt.savefig('lr_finder.jpeg')
+
+
+def update_model(engine, batch):
+    images, masks, weight_maps = batch
+
+    if args.gpu:
+            images = images.cuda()
+            weight_maps = weight_maps.cuda()
+            masks = masks.cuda()
+
+    optimizer.zero_grad()
+    if args.fp16:
+            with autocast():
+
+                gland_preds, aux_preds, out = net(images)
+
+                loss = gland_loss_fn_ce(gland_preds, masks) + \
+                                3 * gland_loss_fn_dice(gland_preds, masks)
+
+                loss_aux = gland_loss_fn_ce(aux_preds, masks) + \
+                                3 * gland_loss_fn_dice(aux_preds, masks)
+
+                weight_maps = weight_maps.float().div(20)
+                loss = loss * weight_maps + 0.4 * loss_aux * weight_maps
+
+
+
+
+                contrasive_loss, xor_mask = contrasive_loss_fn(out, gland_preds, masks, queue=net.queue, queue_ptr=net.queue_ptr, fcs=net.fcs)
+
+                sup_loss = (loss + xor_mask * loss).mean()
+                loss = sup_loss + args.alpha * contrasive_loss
+
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+    return loss.item()
+
+
 
 
 if __name__ == '__main__':
 
+
+    # from gpustats import GPUStats
+    # gpu_stats = GPUStats(gpus_needed=1, sleep_time=60 * 5, exec_thresh=3, max_gpu_mem_avail=0.01, max_gpu_util=0.01)
+    # gpu_stats.run()
+
+
+    print('cccccccccccccccc')
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', type=int, default=5,
+    parser.add_argument('-b', type=int, default=10,
                         help='batch size for dataloader')
-    parser.add_argument('-start_lr', type=float, default=1e-7,
-                        help='initial learning rate')
-    parser.add_argument('-end_lr', type=float, default=10,
-                        help='initial learning rate')
-    parser.add_argument('-stop_div', type=bool, default=True,
-                        help='stops when loss diverges')
-    parser.add_argument('-num_it', type=int, default=100,
-                        help='number of iterations')
-    parser.add_argument('-skip_start', type=int, default=10,
-                        help='number of batches to trim from the start')
-    parser.add_argument('-skip_end', type=int, default=5,
-                        help='number of batches to trim from the end')
-    parser.add_argument('-weight_decay', type=float,
-                        default=0, help='weight decay factor')
-    parser.add_argument('-net', type=str, required=True, help='network name')
-    parser.add_argument('-dataset', type=str, default='Camvid', help='dataset name')
+    parser.add_argument('-net', type=str, required=True, help='if resume training')
+    parser.add_argument('-dataset', type=str, default='Glas', help='dataset name')
+    parser.add_argument('-alpha', type=float, default=0.1, help='panalize parameter')
+    parser.add_argument('-gpu', action='store_true', default=False, help='whether to use gpu')
+    parser.add_argument('-fp16', action='store_true', default=False, help='whether to use mixed precision training')
+    parser.add_argument('-pretrain', action='store_true', default=False, help='pretrain data')
     parser.add_argument('-download', action='store_true', default=False,
         help='whether to download camvid dataset')
-    parser.add_argument('-gpu', action='store_true', default=False, help='whether to use gpu')
-
 
     args = parser.parse_args()
+    print('cccccccccccccccc')
+    print(args)
 
-    #train_dataset = CamVid(
-    #    settings.DATA_PATH,
-    #    'train'
-    #)
+    root_path = os.path.dirname(os.path.abspath(__file__))
 
-    #train_transforms = transforms.Compose([
-    #    transforms.RandomRotation(value=train_dataset.ignore_index),
-    #    transforms.RandomScale(value=train_dataset.ignore_index),
-    #    transforms.RandomGaussianBlur(),
-    #    transforms.RandomHorizontalFlip(),
-    #    transforms.ColorJitter(),
-    #    transforms.Resize(settings.IMAGE_SIZE),
-    #    transforms.ToTensor(),
-    #    transforms.Normalize(settings.MEAN, settings.STD),
-    #])
+    print()
 
-    #train_dataset.transforms = train_transforms
+    train_dataloader = utils.data_loader(args, 'train')
+    train_dataset = train_dataloader.dataset
+    # train_dataset.times = 3
 
-    #train_loader = torch.utils.data.DataLoader(
-    #    train_dataset, batch_size=args.b, num_workers=4)
-    train_loader = utils.data_loader(args, 'train')
-    train_dataset = train_loader.dataset
+    # val_loader = utils.data_loader(args, 'val')
 
-    net = utils.get_model(args.net, 3, train_dataset.class_num)
+    #net = utils.get_model('tgt', 3, train_dataset.class_num, args=args)
+    net = utils.get_model('tg', 3, train_dataset.class_num, args=args)
+
+
+    # eish + crag + glas
+    ckpt_path = '/data/hdd1/by/House-Prices-Advanced-Regression-Techniques/checkpoints/tri_graph_Saturday_25_March_2023_23h_38m_31s/iter_39999.pt'
+
+    print('Loading pretrained checkpoint from {}'.format(ckpt_path))
+    #new_state_dict = utils.on_load_checkpoint(net.state_dict(), torch.load(ckpt_path)['state_dict'])
+    new_state_dict = utils.on_load_checkpoint(net.state_dict(), torch.load(ckpt_path))
+    net.load_state_dict(new_state_dict)
+    print('Done!')
+
     if args.gpu:
         net = net.cuda()
 
-    loss, lr = lr_finder(train_loader, net, start_lr=args.start_lr,
-                         end_lr=args.end_lr, stop_div=args.stop_div,
-                         weight_decay=args.weight_decay)
-    plot(loss, lr, skip_start=args.skip_start, skip_end=args.skip_end)
+    net.train()
+
+    # train(net, train_loader, val_loader, writer, args)
+    # evaluate(net, val_loader, writer, args)
+
+    optimizer = optim.SGD(net.parameters(), lr=1e-07, momentum=0.9, weight_decay=5e-4)
+
+    gland_loss_fn_ce = nn.CrossEntropyLoss(ignore_index=train_dataloader.dataset.ignore_index, reduction='none')
+    gland_loss_fn_dice = DiceLoss(ignore_index=train_dataloader.dataset.ignore_index, reduction='none')
+
+    #cnt_weight = torch.tensor([0.52756701, 9.568812]).cuda()
+    cnt_weight = None
+    cnt_loss_fn_ce = nn.CrossEntropyLoss(weight=cnt_weight, ignore_index=train_dataloader.dataset.ignore_index, reduction='none')
+    cnt_loss_fn_dice = DiceLoss(class_weight=cnt_weight, ignore_index=train_dataloader.dataset.ignore_index, reduction='none')
+    #var_loss_fn = LossVariance()
+
+    contrasive_loss_fn = GlandContrastLoss(8, ignore_idx=train_dataloader.dataset.ignore_index, temperature=0.07)
+
+    #loss_l2 = nn.MSELoss()
+    #loss_seg = SegmentLevelLoss(op=args.op)
+    sampler = _sampler.OHEMPixelSampler(ignore_index=train_dataloader.dataset.ignore_index)
+    #cnt_loss_fn_ce = WeightedLossWarpper(cnt_loss_fn_ce, sampler)
+    #cnt_loss_fn_dice = WeightedLossWarpper(cnt_loss_fn_dice, sampler)
+    gland_loss_fn_ce = WeightedLossWarpper(gland_loss_fn_ce, sampler)
+    gland_loss_fn_dice = WeightedLossWarpper(gland_loss_fn_dice, sampler)
+
+    train_iterloader = IterLoader(train_dataloader)
+    net.train()
+
+
+    scaler = GradScaler()
+
+
+
+    trainer = Engine(update_model)
+    ProgressBar(persist=True).attach(trainer, output_transform=lambda x: {"batch loss": x})
+    lr_finder = FastaiLRFinder()
+    to_save={'model': net, 'optimizer': optimizer}
+    print(trainer.state.epoch_length, trainer.state.max_epochs)
+    # trainer.load_state_dict({
+    #     'epoch_length':1,
+    #     'max_epochs':1
+    # })
+
+    with lr_finder.attach(trainer, to_save, start_lr=1e-7, num_iter=200) as trainer_with_lr_finder:
+        print(type(trainer_with_lr_finder))
+        trainer_with_lr_finder.run(train_dataloader, max_epochs=1, epoch_length=1)
+
+    print("Suggested LR", lr_finder.lr_suggestion())
+
+    ax = lr_finder.plot()
+    ax.figure.savefig("output.jpg")
