e91153eddf02e1eb7ebf1f05b907ae5aad24cd92
weiaicunzai <372995411@qq.com>
2024-06-24 19:34:57 +0800

    save

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=1
 #for i in 1 2 3
 #for i in 1
 #do
@@ -16,5 +16,6 @@ export CUDA_VISIBLE_DEVICES=2
 #python -u train_m.py -net tgt -b 16 -lr 0.004 -min_lr 0.0004  -dataset Glas -gpu -download  -prefix unet_branch_SGD_473 -poly  -fp16 -eval_iter 2000   -alpha 0.1 -wd 0.0001
 #python -u train.py -net tg -b 16 -lr 0.004 -min_lr 0.0004  -dataset Glas -gpu -download  -prefix unet_branch_SGD_473 -poly  -fp16 -eval_iter 2000   -alpha 0.1 -wd 0.0005
 #python -u train.py -net tgt -b 8 -lr 0.004 -min_lr 0.0004  -dataset crag -gpu -download  -prefix unet_branch_SGD_473 -poly  -fp16 -eval_iter 200   -alpha 0.1 -imgset train
-python -u train.py -net tgt -b 8 -lr 0.004 -min_lr 0.0004  -dataset crag -gpu -download  -prefix unet_branch_SGD_473 -poly  -fp16 -eval_iter 200   -alpha 0.1 -imgset train
+#python -u train.py -net tgt -b 8 -lr 0.004 -min_lr 0.0004  -dataset crag -gpu -download  -prefix unet_branch_SGD_473 -poly  -fp16 -eval_iter 200   -alpha 0.1 -imgset train
+python -u train.py -net tgt -b 8 -lr 0.004 -min_lr 0.0004  -dataset Glas -gpu -download  -prefix unet_branch_SGD_473 -poly  -fp16 -eval_iter 200   -alpha 0.1 -imgset train
 #done
